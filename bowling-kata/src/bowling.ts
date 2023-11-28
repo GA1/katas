1751b2df2b5e4264e0f5de58061b199ec83bb745
Kamil Nguyen Van <nvkga1@gmail.com>
2023-11-28 20:55:32 +0100

    add bowling with 4 tests passing

--- bowling-kata/src/bowling.ts
+++ bowling-kata/src/bowling.ts
@@ -1,3 +1,28 @@
-export const bowling = (rolls: string[]): number => {
-  return 300
+export const bowling = (frames: string[]): number => {
+  let score = 0
+  for (let i = 0; i < frames.length; i++) {
+    const frame = frames[i]
+    let framePoints: number
+    let roll1: number
+    let roll2: number
+
+    if (frame[1] === '/') {
+      score += 10 + Number(frames[i + 1][0])
+      continue
+    }
+
+    if (frame[0] === '-') {
+      roll1 = 0
+    } else {
+      roll1 = Number(frame[0])
+    }
+    if (frame[1] === '-') {
+      roll2 = 0
+    } else {
+      roll2 = Number(frame[1])
+    }
+    framePoints = roll1 + roll2
+    score += framePoints
+  }
+  return score
 }
